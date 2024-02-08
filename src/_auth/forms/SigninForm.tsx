70565a855424a6fb36f846da51fca293184573da
ltc01 <laxmichoudhary909@gmail.com>
2024-02-08 16:35:19 +0530

    first commit

---
+++ src/_auth/forms/SigninForm.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const SigninForm = () => {
+  return (
+    <div>
+      <Button className="bg-black text-white">Click Me!</Button>
+    </div>
+  )
+}
+
+export default SigninForm
---
+++ src/_auth/forms/SignupForm.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const SignupForm = () => {
+  return (
+    <div>
+      <Button>Sign up</Button>
+    </div>
+  )
+}
+
+export default SignupForm
